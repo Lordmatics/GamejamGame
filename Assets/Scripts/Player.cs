44111b5a71c9d2940755153a4184eb58bcac6a21
Lordmatics <niallmaple@yahoo.co.uk>
2017-04-30 14:08:33 +0100

    A few basic setup stuff
    Player script
    Player controls - basic

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour
+{
+
+    private PlayerMovement movementScript;
+
+	// Use this for initialization
+	void Start ()
+    {
+        movementScript = GetComponent<PlayerMovement>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        movementScript.PlayerControls();
+        movementScript.PlayerJump();
+	}
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    // Physics based movement
+    private Rigidbody _rigidbody;
+
+    [SerializeField]
+    private Vector3 jumpForce = new Vector3(0.0f, 300.0f, 0.0f);
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.freezeRotation = true;
+    }
+
+    public void PlayerControls()
+    {
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+
+    public void PlayerJump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody.AddForce(jumpForce);
+        }
+    }
+}
