b29dc470fa846c803c71a5bfdf54b7a9b22a5d06
Lordmatics <niallmaple@yahoo.co.uk>
2017-05-07 16:59:45 +0100

    Simple Tool for instantiating nuts made

---
+++ Assets/Editor/Utility/NutSpawnerEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(NutSpawner))]
+public class NutSpawnerEditor : Editor
+{
+
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        NutSpawner myScript = (NutSpawner)target;
+        if (GUILayout.Button("Build Object"))
+        {
+            myScript.BuildObject();
+        }
+
+        //if (GUILayout.Button("Destroy All Objects Spawned"))
+        //{
+        //    myScript.ClearObjectsSpawned();
+        //}
+    }
+}
---
+++ Assets/Scripts/Utility/NutSpawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NutSpawner : MonoBehaviour
+{
+
+    [SerializeField]
+    private Vector3 spawnPoint;
+
+    public void BuildObject()
+    {
+        GameObject spawnedObj = (GameObject)Instantiate(Resources.Load("NutPickup" , typeof(GameObject)), spawnPoint, Quaternion.identity);
+        //if (parents.Length > 0)
+        //    if (parentIndex < parents.Length)
+        //    {
+        //        spawnedObj.transform.parent = parents[parentIndex].transform;
+        //        spawnedObjects.Add(spawnedObj);
+        //    }
+    }
+}
