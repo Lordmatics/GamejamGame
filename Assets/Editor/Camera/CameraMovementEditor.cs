4d43b95bd8ef62a81d4bdc603981516edd6f962b
Lordmatics <niallmaple@yahoo.co.uk>
2017-05-13 20:52:23 +0100

    Created a camera movement script

---
+++ Assets/Editor/Camera/CameraMovementEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(CameraMovement))]
+public class CameraMovementEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        EditorGUILayout.HelpBox("Controls for the camera movement", MessageType.Info);
+
+        base.OnInspectorGUI();
+    }
+
+}
---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speedHorizontal = 2.0f;
+
+    [SerializeField]
+    private float speedVertical = 2.0f;
+
+    [SerializeField]
+    private float yaw = 0.0f;
+
+    [SerializeField]
+    private float pitch = 0.0f;
+
+    [SerializeField]
+    private bool bTest = false;
+
+	void Update ()
+    {
+        if(bTest)
+            CameraRotation();
+	}
+
+    void CameraRotation()
+    {
+        yaw += Input.GetAxis("Mouse X") * speedHorizontal;
+        pitch -= Input.GetAxis("Mouse Y") * speedVertical;
+
+        transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+}
