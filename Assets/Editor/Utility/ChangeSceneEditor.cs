4f21149dd5ba387fb558ff78bf9c528c52c67058
Lordmatics <niallmaple@yahoo.co.uk>
2017-05-18 00:36:24 +0100

    Created a tool for quickly changing scenes on the menu bar at the top
    This will be useful further down the line

---
+++ Assets/Editor/Utility/ChangeSceneEditor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class ChangeSceneEditor : Editor
+{
+
+    [MenuItem("OpenScene/DevScenes/Niall_Scene")]
+    public static void OpenNiallScene()
+    {
+        OpenScene("NiallScene");
+    }
+
+    [MenuItem("OpenScene/DevScenes/Ash_Scene")]
+    public static void OpenAshScene()
+    {
+        OpenScene("AshScene");
+    }
+
+    [MenuItem("OpenScene/DevScenes/Jamie_Scene")]
+    public static void OpenJamieScene()
+    {
+        OpenScene("JamieScene");
+    }
+
+    [MenuItem("OpenScene/DevScenes/Kris_Scene")]
+    public static void OpenKrisScene()
+    {
+        OpenScene("KrisScene");
+    }
+
+    [MenuItem("OpenScene/DevScenes/Test_Scene")]
+    public static void OpenTestScene()
+    {
+        OpenScene("TestScene");
+    }
+
+    static void OpenScene(string name)
+    {
+        if(EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene("Assets/Scenes/DevScenes/" + name + ".unity");
+        }
+    }
+}
