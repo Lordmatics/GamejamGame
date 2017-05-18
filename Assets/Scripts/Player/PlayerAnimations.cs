a1ed01abcaac923f42c84eed053a76e0e9b932e6
Lordmatics <niallmaple@yahoo.co.uk>
2017-05-18 21:19:20 +0100

    Added Kris Animation + Made script ready
    Idle
    Run
    Script

---
+++ Assets/Scripts/Player/PlayerAnimations.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+[AddComponentMenu("Scripts/Player/PlayerAnimations")]
+public class PlayerAnimations : MonoBehaviour
+{
+    private Animator _animator;
+
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+
+
+}
